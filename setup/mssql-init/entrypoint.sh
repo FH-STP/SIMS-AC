1733d20ab47a028c6d87dbd34ee00b0ef9e77581
reloaded <59805910+nik-kl@users.noreply.github.com>
2025-10-08 20:04:47 +0200

    Update entrypoint.sh

--- setup/mssql-init/entrypoint.sh
+++ setup/mssql-init/entrypoint.sh
@@ -8,8 +8,9 @@ initialize_database() {
     sleep 20s
 
     # Run the setup script to create the DB and schema
+    # The sqlcmd command is now in the PATH, so we don't need the full path.
     echo "Running setup.sql to initialize the database..."
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d master -i setup.sql
+    sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d master -i setup.sql
 }
 
 # Run the initialization function in the background
@@ -18,25 +19,4 @@ initialize_database &
 # Start the SQL Server process in the foreground
 # This will be the main process of the container
 /opt/mssql/bin/sqlservr
-```
 
-### ⚠️ **Crucial Next Step: Fixing Line Endings**
-
-After you replace the content of your old `entrypoint.sh` with the code above, you **must** ensure it is saved with Linux-style line endings (`LF`).
-
-If you are using a code editor like **Visual Studio Code**, you can easily change this in the bottom-right corner of the status bar. Click on `CRLF` and change it to `LF`.
-
-
-
-### What to Do Now
-
-1.  Replace the code in your `setup/mssql-init/entrypoint.sh` file with the new version above.
-2.  Ensure the file is saved with `LF` line endings.
-3.  Run the following commands in your terminal to get a fresh start:
-    ```bash
-    # This will remove the old, broken containers and data
-    docker compose down --volumes
-
-    # This will rebuild the mssql-init image with the new script and start everything
-    docker compose up --build -d
-    
